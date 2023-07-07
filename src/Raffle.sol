9395ea56e2634d58e5dd27c3e51839198263c25d
st4rbrain <cosmofi@wsl.com>
2023-07-07 17:02:40 +0530

    initialized Raffle contract

---
+++ src/Raffle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifer: MIT
+
+pragma solidity ^0.8.18;
+
+
+/**
+ * @title A sample Raffle Contract
+ * @author st4rbrain
+ * @notice This contract is for creating a sample Raffle
+ * @dev Implements ChainlinkVRFv2
+ */
+contract Raffle {
+
+}
